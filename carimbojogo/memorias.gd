ff55542abfce00379cd591fde037c343a406b169
AugustoMizu <augusto10barros@gmail.com>
2026-02-20 02:39:05 -0300

    Add recursos de texto para usar nos docs
    texto para usar nos docs de memoria, 24 ao todo
    
    detalhes do como foram feitos estão no segundo  miro

--- carimbojogo/memorias.gd
+++ carimbojogo/memorias.gd
@@ -1,12 +1,19 @@
 extends Resource
 class_name Memoria
 
-enum TipoMemoria {TECNICA, AFETIVA, MOTORA, CRIATIVA, TRAUMATICA}
+enum TipoMemoria {TECNICA, AFETIVA, MOTORA, CRIATIVA}
 
 @export                 var nome_doador: String = ""
 @export                 var tipo: TipoMemoria = TipoMemoria.TECNICA
-@export_range(0, 100)   var pontos_ia: int = 0
-@export_range(0.0, 1.0) var perigo: float = 0.0
-@export                 var carimbo_aprovado: bool
+@export_range(0, 100)   var pontos: int = 0
+@export_range(0.0, 1.0) var perigo: float = 0.0 # nivel de incoerência(alucinações) no resumo da memoria que "corrempe" a "ia",
+												# pode usar esse ou o "resumo_certo", leva ao final BOM 'demitido'
+
+@export                 var carimbo_aprovado: bool # o player carimbou essas memoria como 'aprovado' para enviar
+@export                 var resumo_certo: bool # se true o resumo da memoria feito pela "ia" está correto, leva ao final em que a "ia" ganha player morrea
+											   # se false o resumo tem alucinações, o player deve carimbar como'reprovado'
+
 @export_multiline       var descricao: String = ""
-@export_multiline       var resumo_descricao: String = ""
+@export_multiline       var resumo_descricao: String = "" # resumo da memoria feita pela "ia"
+
+# varios "resumo_certo" false com "carimbo_aprovado" true podem levar ao final BOM, 'demitido'
