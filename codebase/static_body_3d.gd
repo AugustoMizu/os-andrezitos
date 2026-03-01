ec2ff1c160ece6ecabe0f33797eea0859133cf20
GUILHERME KELLER <kellerguilherme15@gmail.com>
2026-03-01 13:43:18 -0300

    update

--- codebase/static_body_3d.gd
+++ codebase/static_body_3d.gd
@@ -1,7 +1,15 @@
 extends StaticBody3D
-
 @onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
-var is_on: bool = false
+var is_on: bool = true
+
+func _ready() -> void:
+	wait(3)
+	await(wait(2))
+	audio_player.play()
+	is_on = true
+	
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
 
 #faz o audio tocarks
 func interact() -> void:
